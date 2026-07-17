90947e6ba29ab385e9554a76ea7143a3d81a977c
BlueCherryB <51278873+BlueCherryB@users.noreply.github.com>
2026-07-17 16:12:32 +0900

    Fix: Add Sanity structure pane IDs
    fix: add Sanity structure pane ids

--- studio/structure.ts
+++ studio/structure.ts
@@ -4,22 +4,27 @@ const caseStudyType = 'caseStudy'
 
 export const structure: StructureResolver = (S) =>
   S.list()
+    .id('content')
     .title('콘텐츠')
     .items([
       S.documentTypeListItem(caseStudyType)
+        .id('caseStudies')
         .title('수행사례')
         .child(
           S.documentTypeList(caseStudyType)
+            .id('publishedCaseStudies')
             .title('수행사례')
             .filter('_type == $type && !(_id in path("drafts.**"))')
             .params({type: caseStudyType})
             .initialValueTemplates([S.initialValueTemplateItem(caseStudyType)]),
         ),
       S.listItem()
+        .id('draftCaseStudies')
         .title('작성 중인 글')
         .schemaType(caseStudyType)
         .child(
           S.documentList()
+            .id('draftCaseStudiesList')
             .title('작성 중인 글')
             .schemaType(caseStudyType)
             .filter('_type == $type && _id in path("drafts.**")')
