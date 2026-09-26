a110abc7d7f7f1f769d1526aa9c910eb2248e774
DESKTOP-C0AGFBF\nymph <nymph2954@gmail.com>
2026-09-26 18:50:24 +0900

    feat: rotate homepage case studies daily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,12 @@ import { ConsultationSection } from "@/components/home/ConsultationSection";
 import { HeroSection } from "@/components/home/HeroSection";
 import { MobileOfficeBanner } from "@/components/home/MobileOfficeBanner";
 import { PracticeSection } from "@/components/home/PracticeSection";
-import { getFeaturedCaseStudies } from "@/lib/content/caseStudies";
+import { getDailyFeaturedCaseStudies } from "@/lib/content/featuredCaseStudies";
 
-const homepageCaseStudyLimit = 5;
+export const revalidate = 3600;
 
 export default async function Home() {
-  const caseStudies = (await getFeaturedCaseStudies()).slice(
-    0,
-    homepageCaseStudyLimit,
-  );
+  const caseStudies = await getDailyFeaturedCaseStudies();
 
   return (
     <main className="flex flex-1 flex-col bg-white text-zinc-950">
